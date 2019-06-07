3c8cf92894c34643c7ce93020a786fe6e4ad0cd3
anthony <anthony24.kwhomes@gmail.com>
2019-06-07 11:19:58 -0700

    fixed a small syntax error in my server file.

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const actionRouter = require('./ActionRouter');
 
 server.use(express.json());
 
-server.use('./api/projects', projectRouter);
-server.use('./api/actions', actionRouter);
+server.use('/api/projects', projectRouter);
+server.use('/api/actions', actionRouter);
 
 server.get('/', (req, res) => {
     res.send('Server is hot!')
